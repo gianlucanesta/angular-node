18eaeec84b0446c111a0135d70c57208cc05935a
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-23 15:57:32 +0200

    Adding first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PostCreateComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
