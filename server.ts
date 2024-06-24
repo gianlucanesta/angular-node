a5e210d71940abe73ceca66311d6a3b5f1a9cd1b
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-24 23:22:51 +0200

    Improving the server.ts Code

--- server.ts
+++ server.ts
@@ -1,20 +1,46 @@
-// const express = require('express');
-// const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
 
-// app.get('/', (req: any, res: any) => {
-//   res.send('Hello World!');
-// });
-
-// const port = 8080;
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
-
-const http = require('http');
+import debug from 'debug';
+import http from 'http';
 const app = require('./backend/app').default;
 
-const port = 8080;
+const normalizePort = (val: string | number): number | string | boolean => {
+  const port: number = typeof val === 'string' ? parseInt(val, 10) : val;
+  if (isNaN(port)) return val;
+  else if (port >= 0) return port;
+  else return false;
+};
 
-const server = http.createServer(app).listen(port, () => {
+const onError = (error: NodeJS.ErrnoException): void => {
+  if (error.syscall !== 'listen') throw error;
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = (): void => {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr?.port;
+  debug('server')('Listening on ' + bind);
+};
+
+const port = normalizePort(process.env['PORT'] || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+server.on('error', onError);
+server.on('listening', onListening);
