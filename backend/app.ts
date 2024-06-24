570423a272d01a521cab9c4df2c98bc62b18d2b0
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-24 23:44:41 +0200

    Fetching Initial Posts

--- backend/app.ts
+++ backend/app.ts
@@ -1,15 +1,21 @@
 import express, { Request, Response, NextFunction } from 'express';
+import Post from '../src/app/posts/post.model';
 
 const app = express();
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  console.log('first middleware');
-  next();
-});
+app.use('/api/posts', (req: Request, res: Response, next: NextFunction) => {
+  const posts: Post[] = [
+    {
+      title: 'First Post',
+      content: 'This is the first post',
+    },
+    {
+      title: 'Second Post',
+      content: 'This is the second post',
+    },
+  ];
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.send('Hello from express!');
-  next();
+  return res.status(200).json({ message: 'Posts fetched successfully', posts });
 });
 
 export default app;
