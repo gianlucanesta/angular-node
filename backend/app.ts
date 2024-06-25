ef8b47ec49ca768fddc89ae5c8e7daaf2941d168
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-25 19:19:27 +0200

    Understanding CORS

--- backend/app.ts
+++ backend/app.ts
@@ -3,6 +3,16 @@ import Post from '../src/app/posts/post.model';
 
 const app = express();
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/api/posts', (req: Request, res: Response, next: NextFunction) => {
   const posts: Post[] = [
     {
