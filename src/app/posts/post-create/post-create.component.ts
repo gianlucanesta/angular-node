bdd2e978be7c58cf2709b0f4c8c7bcbf5ed1fa02
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-27 21:04:18 +0200

    Adding an Image Preview

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -18,6 +18,7 @@ export class PostCreateComponent implements OnInit {
   post!: Post;
   form!: FormGroup;
   isLoading: boolean = false;
+  imagePreview: string = '';
 
   constructor(
     public postsService: PostsService,
@@ -83,7 +84,12 @@ export class PostCreateComponent implements OnInit {
     const file = (event.target as HTMLInputElement).files![0];
     this.form.patchValue({ image: file });
     this.form.get('image')?.updateValueAndValidity();
-    console.log('file', file);
-    console.log('form', this.form);
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+
+    reader.readAsDataURL(file);
   }
 }
