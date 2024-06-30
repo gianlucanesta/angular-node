e52ecdb6d8b4591e60f0125b0f55d52ccc7cd326
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-30 17:47:22 +0200

    Implementing SPA Authentication

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response, NextFunction } from 'express';
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const User = require('../models/user');
+require('dotenv').config();
 
 router.post('/signup', (req: Request, res: Response, next: NextFunction) => {
   bcrypt.hash(req.body.password, 10).then(async (hash: any) => {
@@ -23,6 +25,46 @@ router.post('/signup', (req: Request, res: Response, next: NextFunction) => {
         return res.status(500).json({ message: 'User creation failed!' });
       });
   });
+});
+
+router.post('/login', (req: Request, res: Response, next: NextFunction) => {
+  User.findOne({ email: req.body.email })
+    .then((user: any) => {
+      if (!user) {
+        return res.status(401).json({
+          message: 'Auth failed',
+        });
+      }
+
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then((result: any) => {
+      if (!result) {
+        return res.status(401).json({
+          message: 'Auth failed',
+        });
+      }
+
+      const token = jwt.sign(
+        {
+          email: req.body.email,
+          userId: req.body._id,
+        },
+        process.env['JWT_KEY'] as string,
+        { expiresIn: '1h' }
+      );
+      return res.status(200).json({
+        token: token,
+        expiresIn: 3600,
+        userId: req.body._id,
+      });
+    })
+    .catch((error: any) => {
+      console.error('Error logging in:', error);
+      return res.status(401).json({
+        message: 'Auth failed',
+      });
+    });
 });
 
 module.exports = router;
