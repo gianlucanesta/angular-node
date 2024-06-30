032efa1e299117ab0ccb307485851d0957ed88ea
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-30 15:47:31 +0200

    Adding the Signup Screen

--- backend/routes/posts.ts
+++ backend/routes/posts.ts
@@ -32,14 +32,14 @@ const upload = multer({ storage: storage });
 const router = express.Router();
 
 // Verifica permessi di lettura
-const imagePath = path.join(__dirname, '../../../../backend/images');
-fs.access(imagePath, fs.constants.R_OK, (err) => {
-  if (err) {
-    console.error(`No read access to ${imagePath}`);
-  } else {
-    console.log(`Read access to ${imagePath} confirmed`);
-  }
-});
+// const imagePath = path.join(__dirname, '../../../../backend/images');
+// fs.access(imagePath, fs.constants.R_OK, (err) => {
+//   if (err) {
+//     console.error(`No read access to ${imagePath}`);
+//   } else {
+//     console.log(`Read access to ${imagePath} confirmed`);
+//   }
+// });
 
 router.post(
   '',
@@ -115,35 +115,6 @@ router.put(
     }
   }
 );
-
-// router.get('', async (req: Request, res: Response, next: NextFunction) => {
-//   try {
-//     const pageSize = +req.query['pageSize']!;
-//     const currentPage = +req.query['currentPage']!;
-//     const postQuery = Post.find();
-//     let fetchedPosts: any;
-//     if (pageSize && currentPage) {
-//       const posts = await postQuery
-//         .skip((currentPage - 1) * pageSize)
-//         .limit(pageSize);
-//       return res
-//         .status(200)
-//         .json({ message: 'Posts fetched successfully', posts });
-//     }
-//     postQuery.then((documents: any) => {
-//       return Post.count().then((count: any) => {
-//         return res.status(200).json({
-//           message: 'Posts fetched successfully',
-//           posts: fetchedPosts,
-//           maxPosts: count,
-//         });
-//       });
-//     });
-//   } catch (error) {
-//     console.error('Error fetching posts:', error);
-//     return res.status(500).json({ message: 'Fetching posts failed!' });
-//   }
-// });
 
 router.get('', async (req: Request, res: Response, next: NextFunction) => {
   try {
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
+  isLoading = false;
 
+  onSignup(form: NgForm) {
+    console.log('form', form.value);
+  }
 }
