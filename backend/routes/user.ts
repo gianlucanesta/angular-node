01a7525afe43f0061ec1592837eda010533d72d8
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-06-30 16:42:52 +0200

    Creating a New User Upon Request

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,20 +1,27 @@
 import express, { Request, Response, NextFunction } from 'express';
-
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const User = require('../models/user');
 
-router.post('', async (req: Request, res: Response, next: NextFunction) => {
-  try {
+router.post('/signup', (req: Request, res: Response, next: NextFunction) => {
+  bcrypt.hash(req.body.password, 10).then(async (hash: any) => {
     const user = new User({
       email: req.body.email,
       password: req.body.password,
     });
-    await user.save();
-    return res.status(201).json({ message: 'User created successfully!' });
-  } catch (error) {
-    console.error('Error creating user:', error);
-    return res.status(500).json({ message: 'User creation failed!' });
-  }
+    await user
+      .save()
+      .then(() => {
+        res.status(201).json({
+          message: 'User created successfully!',
+          result: user,
+        });
+      })
+      .catch((error: any) => {
+        console.error('Error creating user:', error);
+        return res.status(500).json({ message: 'User creation failed!' });
+      });
+  });
 });
 
 module.exports = router;
