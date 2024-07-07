c696038d796ba7ab7b117e47424d260b68d61106
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-07-07 15:42:37 +0200

    Using Controllers

--- backend/routes/posts.ts
+++ backend/routes/posts.ts
@@ -1,11 +1,10 @@
-import express, { Request, Response, NextFunction } from 'express';
-import { ObjectId } from 'mongodb';
+import express from 'express';
 import multer from 'multer';
 import fs from 'fs';
 import path from 'path';
 const isAuth = require('../middleware/is-auth');
 
-const Post = require('../models/post');
+const PostController = require('../controllers/posts');
 
 const MIME_TYPE_MAP: { [key: string]: string } = {
   'image/png': 'png',
@@ -42,184 +41,14 @@ const router = express.Router();
 //   }
 // });
 
-router.post(
-  '',
-  isAuth,
-  upload.single('image'),
-  async (req: Request, res: Response, next: NextFunction) => {
-    if (!req.file) {
-      console.log('File not uploaded');
-      return res.status(400).json({ message: 'Image upload failed' });
-    }
-    try {
-      const url = req.protocol + '://' + req.get('host');
-      const images = '/backend/images/';
-      console.log(req.userData);
-      const post = new Post({
-        title: req.body.title,
-        content: req.body.content,
-        imagePath: url + images + req.file.filename,
-        creator: req.userData!.userId,
-      });
+router.post('', isAuth, upload.single('image'), PostController.createPost);
 
-      post.save().then((createdPost: any) => {
-        res.status(201).json({
-          message: 'Post added successfully',
-          post: {
-            ...createdPost,
-            id: createdPost._id,
-          },
-        });
-      });
-      return res;
-    } catch (error) {
-      console.error('Error creating post:', error);
-      return res.status(500).json({ message: 'Creating a post failed!' });
-    }
-  }
-);
+router.put('/:id', isAuth, upload.single('image'), PostController.updatePost);
 
-router.put(
-  '/:id',
-  isAuth,
-  upload.single('image'),
-  async (req: Request, res: Response) => {
-    let imagePath = req.body.imagePath;
-    if (req.file) {
-      const url = req.protocol + '://' + req.get('host');
-      const images = '/backend/images/';
-      imagePath = url + images + req.file.filename;
-    }
+router.get('', PostController.getPosts);
 
-    try {
-      // Cerca il post esistente
-      const existingPost = await Post.findById(req.params['id']);
+router.get('/:id', PostController.getPost);
 
-      // Se il post esiste e ha un'immagine, elimina l'immagine
-      if (existingPost && existingPost.imagePath) {
-        clearImage(path.basename(existingPost.imagePath));
-      }
-
-      // Crea il nuovo post
-      const post = new Post({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content,
-        imagePath: imagePath,
-        creator: req.userData!.userId,
-      });
-
-      // Aggiorna il post nel database
-      await Post.updateOne(
-        { _id: req.params['id'], creator: req.userData!.userId },
-        post
-      ).then((result: any) => {
-        // console.log(result);
-        if (result.modifiedCount > 0) {
-          return res.status(200).json({
-            message: 'Post updated successfully',
-          });
-        } else {
-          return res.status(401).json({ message: 'Not authorized' });
-        }
-      });
-
-      return res.status(200).json({
-        message: 'Post updated successfully',
-      });
-    } catch (error) {
-      console.error('Error updating post:', error);
-      return res.status(500).json({ message: 'Updating post failed!' });
-    }
-  }
-);
-
-router.get('', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const pageSize = +req.query['pageSize']!;
-    const currentPage = +req.query['currentPage']!;
-    const postQuery = Post.find();
-    let fetchedPosts: any;
-    if (pageSize && currentPage) {
-      fetchedPosts = await postQuery
-        .skip((currentPage - 1) * pageSize)
-        .limit(pageSize);
-    } else {
-      fetchedPosts = await postQuery;
-    }
-    const count = await Post.countDocuments();
-    return res.status(200).json({
-      message: 'Posts fetched successfully',
-      posts: fetchedPosts,
-      maxPosts: count,
-    });
-  } catch (error) {
-    console.error('Error fetching posts:', error);
-    return res.status(500).json({ message: 'Fetching posts failed!' });
-  }
-});
-
-router.get('/:id', async (req: Request, res: Response) => {
-  Post.findById(req.params['id'])
-    .then((post: any) => {
-      if (post) {
-        return res.status(200).json(post);
-      }
-      return res.status(404).json({ message: 'Post not found!' });
-    })
-    .catch((error: any) => {
-      console.error('Error fetching post:', error);
-      return res.status(500).json({ message: 'Fetching post failed!' });
-    });
-});
-
-router.delete(
-  '/:id',
-  isAuth,
-  async (req: Request, res: Response, next: NextFunction) => {
-    const postId = req.params['id'];
-
-    if (!ObjectId.isValid(postId)) {
-      return res.status(400).json({ message: 'Invalid post ID' });
-    }
-
-    try {
-      const post = await Post.findById(postId);
-
-      if (!post) {
-        return res.status(404).json({ message: 'Post not found!' });
-      }
-
-      // Elimina l'immagine dal filesystem
-      clearImage(path.basename(post.imagePath));
-
-      // Elimina il documento del post dal database
-      const result = await Post.deleteOne({
-        _id: post._id,
-        creator: req.userData!.userId,
-      });
-
-      if (result.deletedCount > 0) {
-        return res.status(200).json({ message: 'Post and image deleted!' });
-      } else {
-        return res.status(401).json({ message: 'Not authorized' });
-      }
-    } catch (error) {
-      console.error('Error deleting post:', error);
-      return res.status(500).json({ message: 'Failed to delete post' });
-    }
-  }
-);
-
-const clearImage = (filePath: string) => {
-  filePath = path.join(__dirname, '../../../../backend/images', filePath);
-  fs.unlink(filePath, (err) => {
-    if (err) {
-      console.error('Error deleting image:', err);
-    } else {
-      console.log(`Deleted image: ${filePath}`);
-    }
-  });
-};
+router.delete('/:id', isAuth, PostController.deletePost);
 
 module.exports = router;
--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,79 +1,10 @@
-import express, { Request, Response, NextFunction } from 'express';
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+import express from 'express';
 const router = express.Router();
-const User = require('../models/user');
-require('dotenv').config();
 
-router.post('/signup', (req: Request, res: Response, next: NextFunction) => {
-  bcrypt.hash(req.body.password, 10).then(async (hash: any) => {
-    const user = new User({
-      email: req.body.email,
-      password: hash,
-    });
-    console.log('user', user);
-    await user
-      .save()
-      .then(() => {
-        res.status(201).json({
-          message: 'User created successfully!',
-          result: user,
-        });
-      })
-      .catch((error: any) => {
-        console.error('Error creating user:', error);
-        return res.status(500).json({
-          message: 'Invalid authentication credentials!',
-        });
-      });
-  });
-});
+const UserController = require('../controllers/user');
 
-router.post('/login', (req: Request, res: Response, next: NextFunction) => {
-  let fetchedUser: any;
-  User.findOne({ email: req.body.email })
-    .then((user: any) => {
-      if (!user) {
-        return res.status(401).json({
-          message: 'Auth failed',
-        });
-      }
-      fetchedUser = user;
-      // console.log('Fetched User:', fetchedUser);
-      return bcrypt.compare(req.body.password, user.password);
-    })
-    .then((result: any) => {
-      if (!result) {
-        return res.status(401).json({
-          message: 'Auth failed',
-        });
-      }
-      const token = jwt.sign(
-        {
-          email: fetchedUser.email,
-          userId: fetchedUser._id,
-        },
-        process.env['JWT_KEY'] as string,
-        { expiresIn: '1h' }
-      );
-      // console.log('Generated Token:', token);
+router.post('/signup', UserController.createUser);
 
-      // Verify token content
-      const decodedToken = jwt.verify(token, process.env['JWT_KEY'] as string);
-      // console.log('Decoded Token:', decodedToken);
-
-      return res.status(200).json({
-        token: token,
-        expiresIn: 3600,
-        userId: fetchedUser._id, // Include userId in the response
-      });
-    })
-    .catch((error: any) => {
-      console.error('Error logging in:', error);
-      return res.status(401).json({
-        message: 'Invalid authentication credentials!',
-      });
-    });
-});
+router.post('/login', UserController.userLogin);
 
 module.exports = router;
