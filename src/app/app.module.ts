007af39aac1c8df0a96589e9ab07a086be2922cd
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2024-07-07 16:20:49 +0200

    Creating an Angular Material Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCard } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIcon } from '@angular/material/icon';
-import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDialogModule } from '@angular/material/dialog';
 
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
@@ -27,6 +17,7 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from '../../error-interceptor';
 import { ErrorComponent } from './error/error.component';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
@@ -40,22 +31,12 @@ import { ErrorComponent } from './error/error.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    MatInputModule,
-    MatFormFieldModule,
-    BrowserAnimationsModule,
-    MatCard,
-    MatButtonModule,
-    MatToolbarModule,
-    MatIcon,
-    MatExpansionModule,
-    MatAccordion,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    MatDialogModule,
     HttpClientModule,
+    AngularMaterialModule,
   ],
   providers: [
     provideAnimationsAsync(),
